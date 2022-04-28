6645815040ca603e78fe33262817be18bc1688f5
Maarten van Keulen <mcmaartenz@outlook.com>
2022-04-28 11:43:04 +0200

    Geen porten dialog

--- PortSelector.java
+++ PortSelector.java
@@ -7,14 +7,23 @@ public class PortSelector
 		Port.updatePorten();
 		Object[] comPorten = Port.porten;
 
-		Object selectie = JOptionPane.showInputDialog(parent,
-			"Kan niet verbinden op port " + port + ". Probeer opnieuw of annuleer:",
-			"Verbindingsfout",
-			JOptionPane.ERROR_MESSAGE,
-			null,
-			comPorten,
-			comPorten[0]);
-
+		Object selectie;
+		if (comPorten.length == 0)
+		{
+			JOptionPane.showMessageDialog(parent, "Kan niet verbinden met COM. Sluit een Arduino aan en herstart het programma.",
+					"Verbindingsfout", JOptionPane.ERROR_MESSAGE);
+			return null;
+		}
+		else
+		{
+			selectie = JOptionPane.showInputDialog(parent,
+					"Kan niet verbinden op port " + port + ". Probeer opnieuw of annuleer:",
+					"Verbindingsfout",
+					JOptionPane.ERROR_MESSAGE,
+					null,
+					comPorten,
+					comPorten[0]);
+		}
 		if (selectie == null)
 		{
 			return null;
