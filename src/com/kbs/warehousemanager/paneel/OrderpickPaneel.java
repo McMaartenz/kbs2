fa23679a6a45f38b995e79ab01e271774950594f
TinyTani <guus1207@live.nl>
2022-05-13 14:26:17 +0200

    Placeholders + Orderpanel gedesigned

--- src/com/kbs/warehousemanager/paneel/OrderpickPaneel.java
+++ src/com/kbs/warehousemanager/paneel/OrderpickPaneel.java
@@ -2,17 +2,49 @@ package com.kbs.warehousemanager.paneel;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 import javax.swing.*;
 
 public class OrderpickPaneel extends JPanel implements ActionListener
 {
-	/**
-	 * De orderpick paneel
-	 * Dit bevat de schappen, en de orderselectie
-	 */
+	JPanel magazijnPaneel = new MagazijnPaneel();
+	JPanel orderpicker = new Orderpicker();
+
 	public OrderpickPaneel()
 	{
+		super.setLayout(new GridLayout(2, 1));
+		super.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		add(magazijnPaneel);
+		add(orderpicker);
+	}
 
+	public class MagazijnPaneel extends JPanel {
+		public MagazijnPaneel() {
+			setLayout(new GridLayout(5, 5));
+			File file = new File("images");
+			for (String name : file.list()) {
+				JLabel label = new JLabel();
+
+				label.setIcon(new ImageIcon(
+						new ImageIcon("images/" + name).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+				label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				add(label);
+			}
+		}
+	}
+
+	public class Orderpicker extends JPanel implements ActionListener {
+		public Orderpicker(){
+			String[] orderOptions = {"voorbeeldOrder 1", "voorbeeldOrder 2", "voorbeeldOrder 3"};
+			JComboBox orderList = new JComboBox(orderOptions);
+			orderList.addActionListener(this);
+			add(orderList);
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
 	}
 
 	/**
