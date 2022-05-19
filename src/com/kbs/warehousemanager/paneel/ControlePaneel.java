658aab593793f11789a11f1edea0b9d32962ffd2
ChloeBril <Chloevdb05@hotmail.com>
2022-05-19 10:13:45 +0200

    Update ControlePaneel.java
    knoppen aangemaakt

--- src/com/kbs/warehousemanager/paneel/ControlePaneel.java
+++ src/com/kbs/warehousemanager/paneel/ControlePaneel.java
@@ -4,15 +4,40 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class ControlePaneel extends JPanel implements ActionListener
-{
+public class ControlePaneel extends JPanel implements ActionListener {
 	/**
 	 * De controle paneel
 	 * Dit bevat de knoppen om het programma te sturen
 	 */
-	public ControlePaneel()
-	{
+	private final JButton StartButton = new JButton("Start");
+	private final JButton StopButton = new JButton("Stop");
+	private final JButton RepeatButton = new JButton("Herhaal Pickronde");
 
+
+
+	public ControlePaneel() {
+		super.setLayout(new GridLayout(1, 1));
+		super.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		final JLabel bppLabel = new JLabel("BPP-algoritme:");
+		final JLabel boxesLabel = new JLabel("Aantal dozen: ");
+		final JLabel tspLabel = new JLabel("TSP-algoritme");
+
+		add(StartButton);
+		add(StopButton);
+		add(RepeatButton);
+		add(bppLabel);
+		add(boxesLabel);
+
+		String[] tspOptions = {"Brute-force", "Branch-and-Bound", "Nearest Neighbour"};
+		JComboBox tspList = new JComboBox(tspOptions);
+		tspList.addActionListener(this);
+		add(tspList);
+
+		add(tspLabel);
+		String[] bppOptions = {"First Fit", "Next Fit", "Best Fit", "Worst Fit", "First-Fit-Decreasing", "Next-Fit-Decreasing", "Worst-Fit-Decreasing"};
+		JComboBox bppList = new JComboBox(bppOptions);
+		bppList.addActionListener(this);
+		add(bppList);
 	}
 
 	/**
