5f3905b4189c3fdfc72c3dbaaaa77d520abc4cca
TinyTani <guus1207@live.nl>
2022-05-19 10:17:23 +0200

    Images

--- src/com/kbs/warehousemanager/paneel/OrderpickPaneel.java
+++ src/com/kbs/warehousemanager/paneel/OrderpickPaneel.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import java.io.File;
 import javax.swing.*;
 
+<<<<<<< Updated upstream
 public class OrderpickPaneel extends JPanel implements ActionListener
 {
 	JPanel magazijnPaneel = new MagazijnPaneel();
@@ -60,4 +61,58 @@ public class OrderpickPaneel extends JPanel implements ActionListener
 			JButton srcBtn = (JButton)src;
 		}
 	}
+=======
+public class OrderpickPaneel extends JPanel implements ActionListener {
+    JPanel magazijnPaneel = new MagazijnPaneel();
+    JPanel orderpicker = new Orderpicker();
+
+    public OrderpickPaneel() {
+        super.setLayout(new GridLayout(2, 1));
+        super.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        add(magazijnPaneel);
+        add(orderpicker);
+    }
+
+    public class MagazijnPaneel extends JPanel {
+        public MagazijnPaneel() {
+            setLayout(new GridLayout(5, 5));
+            File file = new File("orderpickImages");
+            for (String name : file.list()) {
+                JLabel label = new JLabel();
+
+                label.setIcon(new ImageIcon(
+                        new ImageIcon("orderpickImages/" + name).getImage().getScaledInstance(75, 75, Image.SCALE_DEFAULT)));
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                add(label);
+            }
+        }
+    }
+
+    public class Orderpicker extends JPanel implements ActionListener {
+        public Orderpicker() {
+            String[] orderOptions = {"voorbeeldOrder 1", "voorbeeldOrder 2", "voorbeeldOrder 3"};
+            JComboBox orderList = new JComboBox(orderOptions);
+            orderList.addActionListener(this);
+            add(orderList);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    /**
+     * Verwerk een event
+     *
+     * @param ae Action event: de event dat binnenkomt
+     */
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object src = ae.getSource();
+        if (src instanceof JButton) {
+            JButton srcBtn = (JButton) src;
+        }
+    }
+>>>>>>> Stashed changes
 }
