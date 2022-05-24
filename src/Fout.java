c8e77a748db00fed4945201ce2bcdc884a0ffb96
Maarten van Keulen <mcmaartenz@outlook.com>
2022-05-24 10:54:27 +0200

    merge

--- src/Fout.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Fout
-{
-	public static void toon(JFrame parent, String probleembeschrijving)
-	{
-		JOptionPane.showMessageDialog(parent, probleembeschrijving, "Probleem", JOptionPane.ERROR_MESSAGE);
-	}
-}
