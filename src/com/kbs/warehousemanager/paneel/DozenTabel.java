04d05890f1b3d5cf90f3524f9f988f399af2e99e
TinyTani <93322211+TinyTani@users.noreply.github.com>
2022-05-27 14:58:46 +0200

    Dozentabel toegevoegd

--- src/com/kbs/warehousemanager/paneel/DozenTabel.java
+++ src/com/kbs/warehousemanager/paneel/DozenTabel.java
@@ -2,43 +2,51 @@ package com.kbs.warehousemanager.paneel;
 
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.Arrays;
 
 public class DozenTabel extends JPanel {
 
+    static int header_height = 75;
     static String[][] dozen = new String[4][];
-    String[][] placeholderData ={{"-"}};
-
+    static DefaultTableModel[] tabelModellen = new DefaultTableModel[4];
+    static JTable[] tabellen = new JTable[4];
+    static JScrollPane[] dozenTabellen = new JScrollPane[4];
 
     public DozenTabel() {
         setLayout(new GridLayout(1, 4));
-        JTable tabel;
         for(int i = 0; i < 4; i++){
-            String[] column = {"Doos " + (i+1)};
-            if(!ItemList.items.isEmpty()) {
-                tabel = new JTable(dozen, column);
-            } else {
-                tabel = new JTable(placeholderData, column);
-            }
-            add(tabel);
+            tabelModellen[i] = new DefaultTableModel();
+            tabelModellen[i].addColumn("Doos " + (i+1));
+            tabellen[i] = new JTable(tabelModellen[i]);
+            dozenTabellen[i] = new JScrollPane(tabellen[i]);
+            add(dozenTabellen[i]);
+            tabellen[i].setEnabled(false);
+            tabellen[i].getTableHeader().setPreferredSize(new Dimension(dozenTabellen[i].getWidth(),header_height));
             setVisible(true);
         }
     }
         public static void voegToe(int maxDoosItems){
-            ItemList.getItems();
-            System.out.println("Items: " + ItemList.items);
             for (int doosnr = 1; doosnr <= 4; doosnr++) {
                 dozen[doosnr - 1] = new String[maxDoosItems];
                 for (int i = (doosnr - 1) * maxDoosItems, j = 0; i < Math.min(maxDoosItems * doosnr, ItemList.items.size()); i++, j++) {
                     dozen[doosnr - 1][j] = ItemList.items.get(i);
                 }
-                System.out.println("doos " + doosnr + ": ");
-                for (String item : dozen[doosnr - 1]) {
-                    System.out.print(item + ", ");
+                if(dozen[doosnr - 1] == null) {
+                    assert dozen[doosnr - 1] != null;
+                    for (String item : dozen[doosnr - 1]) {
+                        tabelModellen[doosnr - 1].addRow(new String[]{item});
+                    }
+                } else {
+                    tabelModellen[doosnr - 1].setRowCount(0);
+                    for (String item : dozen[doosnr - 1]) {
+                        tabelModellen[doosnr - 1].addRow(new String[]{item});
                 }
-                System.out.println();
+
+                }
             }
+
         }
     }
 
